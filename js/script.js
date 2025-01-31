0db53f2aa224588f16a61ed1cee34a45f9839551
Bishru125 <bisharu@veuz.in>
2025-01-31 20:06:14 +0530

    third commit

--- js/script.js
+++ js/script.js
@@ -58,6 +58,30 @@ var counted = 0;
             }
 
         });
+
+        $('.product-carousel').owlCarousel({
+          loop: true,
+          margin: 15,
+          nav: false, 
+          dots: true, 
+          autoplay: true, 
+          autoplayTimeout: 3000, 
+          autoplayHoverPause: true, 
+          responsive: {
+              0: {
+                  items: 1
+              },
+              475: {
+                  items: 1
+              },
+              600: {
+                  items: 2
+              },
+              1000: {
+                  items: 4
+              }
+          }
+      });
         
         $('.product-carousel2').owlCarousel({
             loop: true,
